151fb435b2613ddb4c7217544316d11c96bbd688
Olga Moysidou <olgamoysidou@gmail.com>
2019-08-27 10:34:37 +0200

    Add files via upload

--- src/app/reddit-feeds/reddit-feeds.component.ts
+++ src/app/reddit-feeds/reddit-feeds.component.ts
@@ -3,7 +3,6 @@ import { Router } from "@angular/router";
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 
-
 @Component({
   selector: 'app-reddit-feeds',
   templateUrl: './reddit-feeds.component.html',
@@ -11,16 +10,19 @@ import 'rxjs/add/operator/map';
 })
 export class RedditFeedsComponent implements OnInit {
   results;
+  config: any;
+  p: number = 1;
 
   constructor(private http: HttpClient) {}
-  private apiUrl = 'https://www.reddit.com/r/help/top.json?limit=10';
+  private apiUrl = 'https://www.reddit.com/r/help/top.json?';
 
   ngOnInit() {
-    const apiUrl = 'https://www.reddit.com/r/help/top.json?limit=10';
+    const apiUrl = 'https://www.reddit.com/r/help/top.json?';
      this.http.get(apiUrl).subscribe((response:any) =>  { 
       this.results = response.data.children;
        console.log(this.results);
      });
   }
 }
+
 
